ddaa9759230ca2ddab426044a068f5aa396d273b
Chance Neff <crneff84@gmail.com>
2016-11-18 09:51:10 -0800

    change font on main activity and add toast when empty string is input

--- app/src/main/java/com/epicodus/beerguide/MainActivity.java
+++ app/src/main/java/com/epicodus/beerguide/MainActivity.java
@@ -1,13 +1,42 @@
 package com.epicodus.beerguide;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.breweryFindButton) Button mBreweryFindButton;
+    @Bind(R.id.breweryEditText) EditText mBreweryEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface chunkFiveFont = Typeface.createFromAsset(getAssets(), "fonts/Chunkfive.otf");
+        mAppNameTextView.setTypeface(chunkFiveFont);
+
+        mBreweryFindButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String breweryInput = mBreweryEditText.getText().toString();
+                if(breweryInput.equals("")) {
+                    Toast.makeText(MainActivity.this, "Enter a Brewery", Toast.LENGTH_LONG).show();
+                } else {
+
+                }
+            }
+        });
+
     }
 }

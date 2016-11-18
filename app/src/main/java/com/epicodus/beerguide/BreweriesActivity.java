9c7e2a87234dd218145b0de14e9127a58fd5e83b
Chance Neff <crneff84@gmail.com>
2016-11-18 11:21:44 -0800

    add favorites list functionality to breweries activity

--- app/src/main/java/com/epicodus/beerguide/BreweriesActivity.java
+++ app/src/main/java/com/epicodus/beerguide/BreweriesActivity.java
@@ -1,13 +1,49 @@
 package com.epicodus.beerguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class BreweriesActivity extends AppCompatActivity {
+    @Bind(R.id.breweryAddButton) Button mBreweryAddButton;
+    @Bind(R.id.breweryEditText) EditText mBreweryEditText;
+    @Bind(R.id.breweriesListView) ListView mBreweriesListView;
+    private ArrayList<String> breweries = new ArrayList<>();
+    public static final String TAG = "logs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breweries2);
+        ButterKnife.bind(this);
+
+        mBreweryAddButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String breweryInput = mBreweryEditText.getText().toString();
+                if(breweryInput.equals("")) {
+                    Toast.makeText(BreweriesActivity.this, "Enter a Brewery", Toast.LENGTH_LONG).show();
+                } else {
+                    breweries.add(breweryInput);
+                    ArrayAdapter adapter = new ArrayAdapter(BreweriesActivity.this, android.R.layout.simple_list_item_1, breweries);
+                    mBreweriesListView.setAdapter(adapter);
+                }
+            }
+        });
+
+
+
     }
 }

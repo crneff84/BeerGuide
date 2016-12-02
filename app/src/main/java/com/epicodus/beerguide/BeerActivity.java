a31987a99e026b11a06660a40701d68e6876dc12
Chance Neff <crneff84@gmail.com>
2016-12-02 10:15:30 -0800

    add service for beer api call

--- app/src/main/java/com/epicodus/beerguide/BeerActivity.java
+++ app/src/main/java/com/epicodus/beerguide/BeerActivity.java
@@ -3,6 +3,8 @@ package com.epicodus.beerguide;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,16 +12,25 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.epicodus.beerguide.adapters.BeerListAdapter;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class BeerActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.beerFindButton) Button mBeerFindButton;
     @Bind(R.id.beerEditText) EditText mBeerEditText;
-    @Bind(R.id.beerListView) ListView mBeerListView;
-    private ArrayList<String> beers = new ArrayList<>();
+    @Bind(R.id.beerRecyclerView) RecyclerView mBeerRecyclerView;
+
+    private BeerListAdapter mAdapter;
+
+    private ArrayList<Beer> mBeers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +52,31 @@ public class BeerActivity extends AppCompatActivity implements View.OnClickListe
             if(beerInput.equals("")) {
                 Toast.makeText(BeerActivity.this, "Enter a Beer", Toast.LENGTH_LONG).show();
             } else {
-                beers.add(beerInput);
-                ArrayAdapter adapter = new ArrayAdapter(BeerActivity.this, android.R.layout.simple_list_item_1, beers);
-                mBeerListView.setAdapter(adapter);
+                final BeerSearchService beerSearchService = new BeerSearchService();
+
+                beerSearchService.findBeers(beerInput, new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        mBeers = beerSearchService.processResults(response);
+
+                        BeerActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mAdapter = new BeerListAdapter(getApplicationContext(), mBeers);
+                                mBeerRecyclerView.setAdapter(mAdapter);
+                                RecyclerView.LayoutManager layoutManager =
+                                        new LinearLayoutManager(BeerActivity.this);
+                                mBeerRecyclerView.setLayoutManager(layoutManager);
+                                mBeerRecyclerView.setHasFixedSize(true);
+                            }
+                        });
+                    }
+                });
             }
         }
     }

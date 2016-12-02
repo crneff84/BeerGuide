4c3cb7ad48e23ad379d913eaf2f552d67983e02f
Chance Neff <crneff84@gmail.com>
2016-12-02 09:00:26 -0800

    add constants class

---
+++ app/src/main/java/com/epicodus/beerguide/BreweryDbService.java
@@ -0,0 +1,8 @@
+package com.epicodus.beerguide;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class BreweryDbService {
+    public static void searchBreweries(String )
+}
--- app/src/main/java/com/epicodus/beerguide/Constants.java
+++ app/src/main/java/com/epicodus/beerguide/Constants.java
@@ -4,4 +4,8 @@ package com.epicodus.beerguide;
  * Created by Guest on 12/2/16.
  */
 public class Constants {
+    public static final String BREWERY_DB_KEY = BuildConfig.BREWERY_DB_KEY;
+    public static final String BEER_SEARCH_BASE_URL = "http://api.brewerydb.com/v2/search?&type=beer";
+    public static final String BREWERY_SEARCH_BASE_URL = "http://api.brewerydb.com/v2/search?&type=brewery";
+    public static final String SEARCH_QUERY = "q";
 }

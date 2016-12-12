448de902048de12c6f6d713075d7d50522eac2f3
Chance Neff <crneff84@gmail.com>
2016-12-12 09:07:47 -0800

    begin implementation of saving favorite breweries to firebase

---
+++ app/src/main/java/com/epicodus/beerguide/adapters/FirebaseBreweryViewHolder.java
@@ -0,0 +1,8 @@
+package com.epicodus.beerguide.adapters;
+
+/**
+ * Created by Chance on 12/12/16.
+ */
+
+public class FirebaseBreweryViewHolder {
+}
---
+++ app/src/main/java/com/epicodus/beerguide/ui/SavedBreweryListActivity.java
@@ -0,0 +1,12 @@
+package com.epicodus.beerguide.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedBreweryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
